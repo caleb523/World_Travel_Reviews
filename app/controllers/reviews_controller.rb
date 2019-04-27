dd3b1b372cd0989c83e08362bcf439adcf606a91
Davin Thomas <dethomas1@gmail.com>
2019-04-27 22:45:18 +0000

    Modified star-rating system to be able to select stars (instead of entering number between 1 and 5)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -63,4 +63,14 @@ class ReviewsController < ApplicationController
     def review_params
       params.require(:review).permit(:rating, :comment)
     end
+
+    def
+      @reviews = Review.where(city_id: @city.id).order("created_at DESC")
+
+      if @reviews.blank?
+        @avg_review = 0
+      else
+        @avg_review = @reviews.average(:rating).round(2)
+      end
+    end
 end
